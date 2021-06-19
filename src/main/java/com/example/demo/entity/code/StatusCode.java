1738f6b156d953795a5b1711f21e0dbbfc2201ad
wq <www.460256946@qq.com>
2021-06-19 13:14:26 +0800

    no message

--- src/main/java/com/example/demo/entity/code/StatusCode.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.entity.Code;
-
-public class StatusCode {
-    public static final String SUCC = "1";
-
-    public static final String ERR = "2";
-}
--- src/main/java/com/example/demo/entity/resp/SysUserRoleResp.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.entity.Resp;
-
-public class SysUserRoleResp {
-
-}
